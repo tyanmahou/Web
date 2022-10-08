a6964f6de67f99f1995a29631226da41450961f6
tyanmahou <tyanmahou@gmail.com>
2022-10-09 01:17:50 +0900

    init project

---
+++ web/vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    watchOptions :{
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+  }  
+})
