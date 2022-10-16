0f1f88f723aa063bb497b7fc79d4992829cb7f76
tyanmahou <tyanmahou@gmail.com>
2022-10-17 03:39:35 +0900

    update head

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="main">
+    <div class="header">
+        <div class="logo">
+          <img src='../assets/commons/web_logo.png' width="300">
+        </div>
+        <div class="navi">
+        <ul>
+          <li class="current"><a href="#">Top</a></li>
+          <li><a href="#">WORKS</a></li>
+          <li><a href="#">BLOG</a></li>
+          <li><a href="#">ABOUT</a></li>        
+        </ul>
+        </div>
+    </div>
     <h2>コンテンツ</h2>
     <div id="text">aaaaa</div>
-    aa
+    aa<br>
+    aa<br>
+    aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>
     <foot />
   </div>    
 </template>
