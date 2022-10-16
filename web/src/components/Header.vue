b90186dad5fcd723839a054dc2bf7220795261dc
tyanmahou <tyanmahou@gmail.com>
2022-10-17 04:36:08 +0900

    update header

---
+++ web/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <div class="logo">
+      <img src="../assets/commons/web_logo.png" width="300" />
+    </div>
+    <div class="navi">
+      <ul class="core">
+        <li class="current"><a href="#">Top</a></li>
+        <li><a href="#">GAMES</a></li>
+        <li><a href="#">GALLERY</a></li>
+        <li><a href="#">BLOG</a></li>
+        <li><a href="#">ABOUT</a></li>
+      </ul>
+      <ul class="service">
+        <li><a href="https://twitter.com/tyanmahou" target="_blank"><img src="../assets/commons/twitter.png" width="20" /></a></li>
+        <li><a href="https://github.com/tyanmahou" target="_blank"><img src="../assets/commons/github.png" width="20" /></a></li>
+      </ul>      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,36 +1,29 @@
 <template>
   <div class="main">
-    <div class="header">
-        <div class="logo">
-          <img src='../assets/commons/web_logo.png' width="300">
-        </div>
-        <div class="navi">
-        <ul>
-          <li class="current"><a href="#">Top</a></li>
-          <li><a href="#">WORKS</a></li>
-          <li><a href="#">BLOG</a></li>
-          <li><a href="#">ABOUT</a></li>        
-        </ul>
-        </div>
-    </div>
+    <myheader />
     <h2>コンテンツ</h2>
-    <div id="text">aaaaa</div>
+    <div id="text">aaaaa
+    
+    <p>
     aa<br>
     aa<br>
     aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>aa<br>
+    </p>
+    </div>
     <foot />
   </div>    
 </template>
 
 <script>
+import myheader from "@/components/Header.vue";
 import foot from "@/components/Foot.vue";
-
 export default {
   name: "home-view",
   data() {
     return {};
   },
   components: {
+    myheader,
     foot,
   },
 };
