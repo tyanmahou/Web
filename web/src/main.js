0b7a50bcd5b050d87a6c119a672f846589e67fd8
tyanmahou <tyanmahou@gmail.com>
2022-10-17 07:29:38 +0900

    fix main.js

--- web/src/main.js
+++ web/src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 // import ElementUI from 'element-ui';
 import axios from 'axios'
 
-const app = createApp(App)
-            .use(router)
-            .mount('#app');
+const app = createApp(App);
+app.use(router)
+   .mount('#app');
 app.provide('$axios', axios);
