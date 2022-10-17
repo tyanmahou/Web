a4222756e55452a87e25dc4ab34519c4c4c7747a
tyanmahou <tyanmahou@gmail.com>
2022-10-17 13:03:06 +0900

    update home

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,28 +1,37 @@
 <template>
   <myheader />
   <div class="main">
-    <div class="top-bg">
-      <div class="profile">
-        <div class="profile-core">
-        <div class="profile-icon">
-          <img src="../assets/commons/avatar.png" width="200" />
+    <header class="home-header">
+      <div class="header-container">
+        <div class="profile">
+          <div class="profile-core">
+            <div class="profile-icon">
+              <img src="../assets/commons/avatar.png" width="200" />
+            </div>
+            <div class="profile-text">
+              <h3>TYANMAHOU</h3>
+              <span>GAME PROGRAMMER</span>
+            </div>
+          </div>
         </div>
-        <div class="profile-text">
-          <h3>TYANMAHOU</h3>
-          GAME PROGRAMMER
+        <div class="catch-copy">
+          <h1>MISSION</h1>
+          <p>
+            <span class="big"
+              ><ruby><rb>世界</rb><rt>せかい</rt></ruby></span
+            >
+            をつくる<span class="big2"
+              ><ruby><rb>創作活動</rb><rt>モノづくり</rt></ruby></span
+            >
+          </p>
         </div>
-        </div>           
       </div>
-      <div class="catch-copy">
-      <h1>MISSION</h1>
-      <p>
-        <span class="big"><ruby><rb>世界</rb><rt>せかい</rt></ruby></span> をつくる<span class="big2"><ruby><rb>創作活動</rb><rt>モノづくり</rt></ruby></span>
-      </p>
+    </header>
+    <div class="home-container">
+      <div class = "main-container">
+      <h2>NEWS</h2>
+      <div id="text"></div>
       </div> 
-    </div>
-
-    <h2>NEWS</h2>
-    <div id="text">
     </div>
   </div>
   <foot />
@@ -44,48 +53,38 @@ export default {
 </script>
 
 <style lang="scss">
-.catch-copy{
-  margin: calc(50% - 50vw + 45px) 0 0 100px;
-  padding: 20px;
-  background:#00000080;
-  h1 {
-    text-decoration:underline;
-  }
-  p{
-    padding: 50px 0 60px 40px;
-    font-size: 2.5em;
-
-    span.big {
-      font-size: 2.0em;
-    }
-    span.big2 {
-      padding: 0 0 0 0px;
-      font-size: 2.0em;
-    }    
-  }
-  rt{
-    font-size: 0.3em;
-  }
+.header-container {
+  position: fixed;
+  
+  padding: 100px 0 0 0;
+  width: 100%;
+  color: #ffffff;
 }
-.top-bg {
-  background-image: url("../assets/commons/home_top.jpg");
-  background-attachment: fixed;
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-
-  color: #fff;
+.home-header {
+  width: 100%;
   height: 100vh;
-  margin: -120px calc(50% - 50vw) 20px;
-  padding: 120px 0 0 10px;
+  position: relative;
 }
+
+.home-header:before {
+  content: "";
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  width: 100%;
+  height: 100vh;
+  background: url("../assets/commons/home_top.jpg") no-repeat center;
+  background-size: cover;
+}
+
 .profile {
   display: flex;
   justify-content: flex-end;
   text-align: center;
   padding: 20px 200px 0 0;
-  margin: calc(50% - 50vw + 60px) 0;
-  float:right;
+  margin: 0 0;
+  float: right;
 }
 .profile-icon {
   margin: auto;
@@ -99,6 +98,37 @@ export default {
 }
 .profile-text {
   margin: 10px 0 0 0;
-  background:#00000080;  
+  background: #00000080;
+}
+
+.catch-copy {
+  margin: 0 0 0 100px;
+  padding: 20px;
+  background: #00000080;
+  h1 {
+    text-decoration: underline;
+  }
+  p {
+    padding: 50px 0 60px 40px;
+    font-size: 2.5em;
+
+    span.big {
+      font-size: 2em;
+    }
+    span.big2 {
+      padding: 0 0 0 0px;
+      font-size: 2em;
+    }
+  }
+  rt {
+    font-size: 0.3em;
+  }
+}
+
+.home-container {
+  position: relative;
+  z-index: 1;
+  background: #f5f5dc;
+  height: 600px;
 }
 </style>
