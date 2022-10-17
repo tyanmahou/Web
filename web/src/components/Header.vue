c0ff63aef1f3852613a815811cd8a446a3220a49
tyanmahou <tyanmahou@gmail.com>
2022-10-18 06:45:24 +0900

    update header

--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -1,34 +1,35 @@
 <template>
   <div class="header">
-      <a href="#">
-    <div class="logo">
-    <mylink url="/">
-        <img src="../assets/commons/web_logo.png" width="300" />
+    <a href="#">
+      <div class="logo">
+        <mylink :url="current == 0 ? '#' : '/'" :absolute="current == 0">
+          <img src="../assets/commons/web_logo.png" width="300" />
         </mylink>
-    </div>
-      </a>
+      </div>
+    </a>
     <div class="navi">
       <ul class="core">
-        <li v-for="(item, index) in navis" :key="item.name"
-        v-bind:class="current == index ? 'current' : ''"
+        <li
+          v-for="(item, index) in navis"
+          :key="item.name"
+          v-bind:class="current == index ? 'current' : ''"
         >
-        <a v-if="item.external"
-        :href="item.url"
-        target="_blank"
-        >
-         {{item.name}} <img src="@/assets/commons/icon/external_link.png" width="15" />
-         </a>        
-        <mylink v-else
-        :url="current == index ?'#' : item.url"
-        :absolute="current == index"
-        >
-         {{item.name}}
-         </mylink>
+          <a v-if="item.external" :href="item.url" target="_blank">
+            {{ item.name }}
+            <img src="@/assets/commons/icon/external_link.png" width="15" />
+          </a>
+          <mylink
+            v-else
+            :url="current == index ? '#' : item.url"
+            :absolute="current == index"
+          >
+            {{ item.name }}
+          </mylink>
         </li>
       </ul>
       <ul class="service">
         <li v-for="item in services">
-            <serviceicon :url="item.url" :icon="item.icon" />
+          <serviceicon :url="item.url" :icon="item.icon" />
         </li>
       </ul>
     </div>
@@ -41,7 +42,7 @@ import mylink from "@/components/Link.vue";
 export default {
   name: "Header",
   props: {
-    current: String
+    current: String,
   },
   components: {
     serviceicon,
@@ -49,52 +50,128 @@ export default {
   },
   data() {
     return {
-      navis:[
+      navis: [
         {
-          name:'TOP',
-          url:'/',
-          external: false
+          name: "TOP",
+          url: "/",
+          external: false,
         },
         {
-          name:'GAME',
-          url:'/game',
-          external: false
+          name: "GAME",
+          url: "/game",
+          external: false,
         },
         {
-          name:'GALLERY',
-          url:'/gallery',
-          external: false
+          name: "GALLERY",
+          url: "/gallery",
+          external: false,
         },
         {
-          name:'BLOG',
-          url:'https://mahou-ptr.hatenablog.com',
-          external: true
+          name: "BLOG",
+          url: "https://mahou-ptr.hatenablog.com",
+          external: true,
         },
         {
-          name:'ABOUT',
-          url:'/about',
-          external: false
+          name: "ABOUT",
+          url: "/about",
+          external: false,
         },
       ],
-      services:[
+      services: [
         {
-          url:'https://twitter.com/tyanmahou',
-          icon:'twitter.png'
+          url: "https://twitter.com/tyanmahou",
+          icon: "twitter.png",
         },
         {
-          url:'https://github.com/tyanmahou',
-          icon:'github.png'
+          url: "https://github.com/tyanmahou",
+          icon: "github.png",
         },
         {
-          url:'https://soundcloud.com/tyanmahou',
-          icon:'soundcloud.png'
+          url: "https://soundcloud.com/tyanmahou",
+          icon: "soundcloud.png",
         },
         {
-          url:'https://www.youtube.com/user/tyanmahou',
-          icon:'youtube.png'
+          url: "https://www.youtube.com/user/tyanmahou",
+          icon: "youtube.png",
         },
-      ]
+      ],
     };
-  }
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.logo {
+  margin: 0 0 0 200px;
+  padding: 15px 0 0 0;
+  height: 85px;
+  width: 300px;
+  background: #ffffff;
+  border-radius: 0 0 30px 30px;
+  float: left;
+
+  transition: all 0.2s;
+  &:hover {
+    padding: 20px 0 0 0;
+  }
+}
+
+.navi {
+  padding: 20px 120px 0 0;
+  color: #ffffff;
+  // 右寄せ
+  display: flex;
+  justify-content: flex-end;
+
+  ul.core {
+    display: flex;
+    list-style: none;
+
+    li {
+      margin-bottom: 20px;
+
+      a {
+        display: block;
+        padding: 0 10px;
+        text-decoration: none;
+        color: #ffffff;
+
+        /*線の基点とするためrelativeを指定*/
+        position: relative;
+      }
+      a::after {
+        content: "";
+        /*絶対配置で線の位置を決める*/
+        position: absolute;
+        bottom: 0;
+        left: 10%;
+        /*線の形状*/
+        width: 80%;
+        height: 2px;
+        background: #ffffff;
+        /*アニメーションの指定*/
+        transition: all 0.3s;
+        transform: scale(0, 1);
+        /*X方向0、Y方向1*/
+        transform-origin: center top;
+        /*上部中央基点*/
+      }
+      /*現在地とhoverの設定*/
+      &.current a::after,
+      a:hover::after {
+        transform: scale(1, 1);
+        /*X方向にスケール拡大*/
+      }
+    }
+  }
+  ul.service {
+    display: flex;
+    list-style: none;
+
+    li {
+      margin-bottom: 20px;
+      padding: 0 3px;
+    }
+  }
+}
+</style>
