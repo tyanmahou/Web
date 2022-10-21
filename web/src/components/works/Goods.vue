64a31d425f71f92e6eb37fc137f99e70881742d4
tyanmahou <tyanmahou@gmail.com>
2022-10-22 06:32:07 +0900

    update work goods

--- web/src/components/works/Goods.vue
+++ web/src/components/works/Goods.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="contents-wrap">
-    Comming Soon
+    <h2>LINEスタンプ</h2>
+    <div class="text">
+      <h3>まほうくん スタンプ</h3>
+      <div>
+        <a href="https://store.line.me/stickershop/product/13692750" target="_blank">
+          <img src="@/assets/works/goods/mahou-kun-stamp.png">
+        </a>
+      </div>
+    </div>
   </div>
 </template>
   
@@ -18,6 +26,16 @@ export default {
 </script>
   
 <style lang="scss" scoped>
+.text {
+  border-radius: 25px;
+}
 
+a img {
+  transition: all 0.3s;
+
+  &:hover {
+    transform: scale(0.95);
+  }
+}
 </style>
   
