57aa3534b6468f3251c495c05a43faf944f14ac7
tyanmahou <tyanmahou@gmail.com>
2022-10-24 20:35:41 +0900

    fix burger

--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -249,6 +249,8 @@ export default {
           padding: 15px 0;
           border-bottom: 1px solid $color-theme;
           a {
+            margin: 0;
+            padding: 0;
             width: 100%;
             height: 100%;
           }
