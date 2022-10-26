e0cf70f94a73b9bc06fdd68edb504233ab69e357
tyanmahou <tyanmahou@gmail.com>
2022-10-26 23:36:37 +0900

    add caption anime

---
+++ web/src/components/FigCaption.vue
@@ -0,0 +1,67 @@
+<template>
+  <figure>
+    <slot />
+    <figcaption>
+      <h4>{{title}}</h4>
+      <p>{{caption}}</p>
+    </figcaption>
+  </figure>
+</template>
+    
+<script>
+export default {
+  name: "FigCaption",
+  props: {
+    title: String,
+    caption: String,
+  }
+};
+</script>
+    
+<style lang="scss" scoped>
+figure {
+  min-height: 120px;
+  background: #fff;
+  color: #fff;
+  display: inline-block;
+  position: relative;
+  overflow: hidden;
+
+  :deep(img) {
+    vertical-align: middle;
+  }
+  
+  figcaption {
+    h4,
+    p {
+      position: absolute;
+      left: 0;
+      z-index: 2;
+      width: 100%;
+      height: 40px;
+      line-height: 40px;
+      background: rgba(0, 0, 0, .6);
+      transition: all .3s;
+    }
+
+    h4 {
+      top: -40px;
+    }
+
+    p {
+      bottom: -40px;
+    }
+  }
+
+  &:hover figcaption {
+    h4 {
+      top: 0;
+    }
+
+    p {
+      bottom: 0;
+    }
+  }
+}
+</style>
+    
--- web/src/components/works/Program.vue
+++ web/src/components/works/Program.vue
@@ -1,10 +1,23 @@
 <template>
+  <h2>GAME</h2>
+  <div class="contents-wrap">
+    Comming Soon
+  </div>
+  <h2>WEB</h2>
+  <div class="contents-wrap">
+    <mycaption title="ss" caption="aaa">
+      <img src="@/assets/commons/web_logo.png" width="300">
+    </mycaption>
+  </div>
+  <h2>Library</h2>
   <div class="contents-wrap">
     Comming Soon
   </div>
 </template>
   
 <script>
+import mycaption from "@/components/FigCaption.vue"
+
 export default {
   name: "Program",
   data() {
@@ -12,6 +25,7 @@ export default {
     };
   },
   components: {
+    mycaption
   },
 };
 </script>
