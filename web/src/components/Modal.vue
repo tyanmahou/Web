b5e3bf32e3e2718e06235b5e9980f3f9aecc5bf9
tyanmahou <tyanmahou@gmail.com>
2022-10-27 06:20:11 +0900

    add close btn

--- web/src/components/Modal.vue
+++ web/src/components/Modal.vue
@@ -3,12 +3,14 @@
     <slot name="title"></slot>
   </a>
   <teleport to="body">
-    <transition name="fade" 
-    @before-enter="beforeEnter"
-    @after-leave="afterLeave"> 
-      <div v-if="isOpen" class="modal-window" @click.self="open(false)">
+    <transition name="fade" @before-enter="beforeEnter" @after-leave="afterLeave">
+      <div v-if="isOpen" class="modal-window" @click="open(false)">
         <div class="modal-content">
           <slot name="contents"></slot>
+        </div>
+
+        <div class="close-btn" @click="open(false)">
+          <span /><span />
         </div>
       </div>
     </transition>
@@ -32,7 +34,7 @@ export default {
     },
     beforeEnter(el) {
       document.body.setAttribute("style", `overflow: hidden`);
-    },    
+    },
     afterLeave(el) {
       document.body.setAttribute("style", `overflow: scroll`);
     }
@@ -41,6 +43,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@/scss/common";
+
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.3s ease;
@@ -67,8 +71,47 @@ export default {
   text-align: center;
 
   & .modal-content {
-    display: inline-block;    
+    display: inline-block;
     margin: 60px 20px 60px 20px;
+  }
+}
+
+.close-btn {
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  cursor: pointer;
+  transition: all .3s;
+  &:hover{
+    background: $color-text-light;
+
+    span {
+      background: $color-theme;
+    }
+  }
+  span {
+    display: inline-block;
+
+    position: absolute;
+    height: 7px;
+    border-radius: 2px;
+    background: #A0A0A0;
+    width: 70%;
+
+    &:nth-of-type(1) {
+      top: 22.5px;
+      left: 7.5px;
+      transform: rotate(-45deg);
+    }
+
+    &:nth-of-type(2) {
+      top: 22.5px;
+      left: 7.5px;
+      transform: rotate(45deg);
+    }
   }
 }
 </style>
