9a1edb5f796beb38272f157b2ecadd43fdb17d6e
tyanmahou <tyanmahou@gmail.com>
2022-10-28 02:39:00 +0900

    add SkillIconAutoCategory

---
+++ web/src/components/SkillIconAutoCategory.vue
@@ -0,0 +1,32 @@
+<template>
+  <skillicon :category="master[name].category" :level="level">
+  {{name}}
+  </skillicon>
+</template>
+  
+<script>
+import skillicon from "@/components/SkillIcon.vue";
+import skilldata from "@/data/master/skills";
+export default {
+  name: "SkillIconAutoCategory",
+  props: {
+    name: String,
+    level: {
+      type: Number,
+      default: 1,
+    },
+  },
+  components: {
+    skillicon,
+  },
+  data() {
+    return {
+      master: skilldata
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
+  
---
+++ web/src/data/master/skills.js
@@ -0,0 +1,131 @@
+export default{
+    "C++": {
+      name:"C++", category:"lang"
+    },
+    "C":{
+      name:"C", category:"lang"
+    },        
+    "DirectX": {
+      name:"DirectX", category:"framework"
+    },
+    "Windows API": {
+      name:"Windows API", category:"framework"
+    },
+    "OpenSiv3D": {
+        name:"Siv3D", category:"framework"
+    },    
+    "Siv3D": {
+      name:"Siv3D", category:"framework"
+    },
+    "HLSL": {
+      name:"HLSL", category:"lang"
+    },
+    "C#": {
+      name:"C#", category:"lang"
+    },
+    "Unity": {
+      name:"Unity", category:"framework"
+    },
+    "GLSL": {
+      name:"GLSL", category:"lang"
+    },        
+    "Visual Studio": {
+      name:"Visual Studio", category:"tool"
+    },
+    "VS Code": {
+      name:"VS Code", category:"tool"
+    },
+    "Git": {
+      name:"Git", category:"system"
+    },
+    "GitHub": {
+      name:"GitHub", category:"service"
+    },
+    "SVN": {
+      name:"SVN", category:"system"
+    },
+    "Docker": {
+      name:"Docker", category:"system"
+    },    
+    "Vagrant": {
+      name:"Vagrant", category:"system"
+    },             
+    "PHP": {
+      name:"PHP", category:"lang"
+    },
+    "Phalcon": {
+      name:"Phalcon", category:"framework"
+    },
+    "Laravel": {
+      name:"Laravel", category:"framework"
+    },
+    "Zephir": {
+      name:"Zephir", category:"lang"
+    },        
+    "IntelliJ": {
+      name:"IntelliJ", category:"tool"
+    },   
+    "SQL": {
+      name:"SQL", category:"lang"
+    },             
+    "MySQL" :{
+      name:"MySQL", category:"system"
+    },
+    "SQLite":{
+      name:"SQLite", category:"system"
+    },
+    "Redis": {
+      name:"Redis", category:"system"
+    },
+    "AWS" :{
+      name:"AWS", category:"service"
+    },
+    "CircleCI":{
+      name:"CircleCI", category:"service"
+    },
+    "HTML":{
+      name:"HTML", category:"lang"
+    },
+    "css": {
+      name:"css", category:"lang"
+    },
+    "sass": {
+      name:"sass", category:"lang"
+    },
+    "JavaScript": {
+      name:"JavaScript", category:"lang"
+    },
+    "Vue.js": {
+      name:"Vue.js", category:"framework"
+    },
+    "Go": {
+      name:"Go", category:"lang"
+    },
+    "Java": {
+      name:"Java", category:"lang"
+    },
+    "Android Studio": {
+      name:"Android Studio", category:"tool"
+    },
+    "cocos2d-x": {
+      name:"cocos2d-x", category:"framework"
+    },            
+    "XCode": {
+      name:"XCode", category:"tool"
+    },            
+    "CocosCreater":{
+      name:"CocosCreater", category:"framework"
+    },
+    "Microsoft 365": {
+      name:"Microsoft 365", category:"tool"
+    },       
+    "MATLAB": {
+      name:"MATLAB", category:"lang"
+    },                  
+    "Prolog": {
+      name:"Prolog", category:"lang"
+    },
+    "Perl": {
+      name:"Perl", category:"lang"
+    },         
+};
