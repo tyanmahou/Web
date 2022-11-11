93e58127ae6a89c1ee6d5ee5d59bf7578fe9558f
tyanmahou <tyanmahou@gmail.com>
2022-11-12 01:42:08 +0900

    update dice developer

--- web/src/components/works/program/game/DiceDeveloper.vue
+++ web/src/components/works/program/game/DiceDeveloper.vue
@@ -1,21 +1,25 @@
 <template>
     <workbase>
         <h2>DiceDeveloper</h2>
-        <div>
-            <slideshow width="400" :slide="slide" />
-        </div>
         <div class="skill">
             <skillicon v-for="s in skills" :name="s" />
         </div>
-        <div class="text">
-            サイコロの展開図をテーマにしたパズルゲーム
-            <br /><br/>
-            初めて作ったスマホアプリです。<br/>
-            マスの目を指で動かして他のマスにもっていくと数字が足されます。<br/>(6を越えたら余剰の値になります)<br/>
-            展開図の形で1~6の数字を揃えるとマスが消えてスコアが加算されます。<br/>
-            Twitterなどへのスコアシェアの機能もいれてみました。<br/>
-            部活動で作った作品です。
+        <div class="about">
+            <div class="to-left">
+                <h3>DESCRIPTION</h3>
+            </div>
+
+            <div class="text">
+                サイコロの展開図をテーマにしたパズルゲーム
+                <br /><br />
+                初めて作ったスマホアプリです。<br />
+                マスの目を指で動かして他のマスにもっていくと数字が足されます。<br />(6を越えたら余剰の値になります)<br />
+                展開図の形で1~6の数字を揃えるとマスが消えてスコアが加算されます。<br />
+                Twitterなどへのスコアシェアの機能もいれてみました。<br />
+                部活動で作った作品です。
+            </div>
         </div>
+        <media :slide="slide" width="400" portrait />
         <div class="to-left">
             <h3>DEVELOP</h3>
         </div>
@@ -33,7 +37,7 @@ import workbase from "@/components/works/program/WorkBase.vue"
 import autolink from "@/components/AutoLink.vue"
 import skillicon from "@/components/SkillIconAutoCategory.vue"
 import youtube from "@/components/widgets/Youtube/Embed.vue"
-import slideshow from "@/components/Slideshow.vue"
+import media from "@/components/works/program/MediaGallery.vue"
 
 export default {
     name: "DiceDeveloper",
@@ -54,7 +58,7 @@ export default {
     components: {
         autolink,
         skillicon,
-        slideshow,
+        media,
         youtube,
         workbase
     },
