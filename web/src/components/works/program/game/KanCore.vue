64a839f3122c576349fbbf931d1328beeeb0f130
tyanmahou <tyanmahou@gmail.com>
2022-11-14 23:39:36 +0900

    update KanCore

--- web/src/components/works/program/game/KanCore.vue
+++ web/src/components/works/program/game/KanCore.vue
@@ -1,19 +1,26 @@
 <template>
     <workbase>
         <h2>KanCore</h2>
-        <div>
-            <img src="@/assets/works/program/game/KanCore/pic99.gif" width ="600"/>
-            <slideshow width="900" :slide="slide" />
-        </div>
         <div class="skill">
             <skillicon v-for="s in skills" :name="s" />
         </div>
-        <div class="text">
-            アルミ缶の中心をクリックするゲーム
-            <br /><br/>
-            初めてSiv3Dで作ったゲームです。<br/>
-            缶のより中心をクリックすることで高得点を狙えるので時間内でどれだけクリックしてスコアを稼げるか競います。
+        <div class="about">
+            <div class="desc">
+                <div class="to-left">
+                    <h3>DESCRIPTION</h3>
+                </div>
+                <div class="text">
+                    アルミ缶の中心をクリックするゲーム
+                    <br /><br />
+                    初めてSiv3Dで作ったゲームです。<br />
+                    缶のより中心をクリックすることで高得点を狙えるので時間内でどれだけクリックしてスコアを稼げるか競います。
+                </div>
+            </div>
+            <div class="top-media">
+                <img src="@/assets/works/program/game/KanCore/pic99.gif" width="600" />
+            </div>
         </div>
+        <media :slide="slide" />
         <div class="to-left">
             <h3>DEVELOP</h3>
         </div>
@@ -28,7 +35,7 @@ import workbase from "@/components/works/program/WorkBase.vue"
 import autolink from "@/components/AutoLink.vue"
 import skillicon from "@/components/SkillIconAutoCategory.vue"
 import youtube from "@/components/widgets/Youtube/Embed.vue"
-import slideshow from "@/components/Slideshow.vue"
+import media from "@/components/works/program/MediaGallery.vue"
 
 export default {
     name: "KanCore",
@@ -48,7 +55,7 @@ export default {
     components: {
         autolink,
         skillicon,
-        slideshow,
+        media,
         youtube,
         workbase
     },
