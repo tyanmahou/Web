30a289c272cc5bea48d35a001bc7447f12cf69ea
tyanmahou <tyanmahou@gmail.com>
2022-11-15 00:00:39 +0900

    update TakaraSagashi

--- web/src/components/works/program/game/TakaraSagashi.vue
+++ web/src/components/works/program/game/TakaraSagashi.vue
@@ -1,16 +1,23 @@
 <template>
     <workbase>
         <h2>宝さがし</h2>
-        <div>
-            <img src="@/assets/works/program/game/TakaraSagashi/pic99.gif" width="600" />
-            <slideshow width="900" :slide="slide" />
-        </div>
         <div class="skill">
             <skillicon v-for="s in skills" :name="s" />
         </div>
-        <div class="text">
-            アイトラッキング「TobiiEyeX」を使用して視線操作で宝を探すゲームです。
+        <div class="about">
+            <div class="desc">
+                <div class="to-left">
+                    <h3>DESCRIPTION</h3>
+                </div>
+                <div class="text">
+                    アイトラッキング「TobiiEyeX」を使用して視線操作で宝を探すゲームです。
+                </div>
+            </div>
+            <div class="top-media">
+                <img src="@/assets/works/program/game/TakaraSagashi/pic99.gif" width="600" />
+            </div>
         </div>
+        <media :slide="slide" />
         <div class="to-left">
             <h3>DEVELOP</h3>
         </div>
@@ -25,7 +32,7 @@ import workbase from "@/components/works/program/WorkBase.vue"
 import autolink from "@/components/AutoLink.vue"
 import skillicon from "@/components/SkillIconAutoCategory.vue"
 import youtube from "@/components/widgets/Youtube/Embed.vue"
-import slideshow from "@/components/Slideshow.vue"
+import media from "@/components/works/program/MediaGallery.vue"
 
 export default {
     name: "TakaraSagashi",
@@ -46,7 +53,7 @@ export default {
     components: {
         autolink,
         skillicon,
-        slideshow,
+        media,
         youtube,
         workbase
     },
