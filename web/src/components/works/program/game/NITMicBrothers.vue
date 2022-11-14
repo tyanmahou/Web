c10911fc29a4f90737f5d91d46c292512f3ea885
tyanmahou <tyanmahou@gmail.com>
2022-11-15 00:32:37 +0900

    update

--- web/src/components/works/program/game/NITMicBrothers.vue
+++ web/src/components/works/program/game/NITMicBrothers.vue
@@ -5,27 +5,22 @@
             <skillicon v-for="s in skills" :name="s" />
         </div>
         <div class="about">
-            <div class="desc">
-                <div class="to-left">
-                    <h3>DESCRIPTION</h3>
-                </div>
-                <div class="text">
-                    最大4人プレイの乱闘アクションゲームです。<br />
-                    使用キャラクターは部内の作品で生まれたキャラクターを集結しています。<br /><br />
-
-                    じゃんぷして相手の頭を踏みつぶせば倒せてスコアが入ります。一番スコアを稼いだ人の勝ちです。<br />
-                    一度やられてもリスポーンするので、最後まで諦めず遊べます。<br /><br />
-
-                    部活動で学祭に展示した作品です。<br />
-                    その際の評価として以下を得ています。
-                    <ul>
-                        <li>遊んでもらえたランキング1位</li>
-                        <li>面白かったゲームランキング1位</li>
-                    </ul>
-                </div>
+            <div class="to-left">
+                <h3>DESCRIPTION</h3>
             </div>
-            <div class="top-media">
-                <img src="@/assets/works/program/game/NITMicBrothers/pic01.jpg" width="600" />
+            <div class="text">
+                最大4人プレイの乱闘アクションゲームです。<br />
+                使用キャラクターは部内の作品で生まれたキャラクターを集結しています。<br /><br />
+
+                じゃんぷして相手の頭を踏みつぶせば倒せてスコアが入ります。一番スコアを稼いだ人の勝ちです。<br />
+                一度やられてもリスポーンするので、最後まで諦めず遊べます。<br /><br />
+
+                部活動で学祭に展示した作品です。<br />
+                その際の評価として以下を得ています。
+                <ul>
+                    <li>遊んでもらえたランキング1位</li>
+                    <li>面白かったゲームランキング1位</li>
+                </ul>
             </div>
         </div>
         <media :slide="slide" />
