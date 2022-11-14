50acb97c1bcaedc2111137a8d4392d157c295636
tyanmahou <tyanmahou@gmail.com>
2022-11-14 23:56:36 +0900

    update Hohei

--- web/src/components/works/program/game/TimeTravelerHohei.vue
+++ web/src/components/works/program/game/TimeTravelerHohei.vue
@@ -1,20 +1,27 @@
 <template>
     <workbase>
         <h2>タイムトラベラー歩兵</h2>
-        <div>
-        <img src="@/assets/works/program/game/TimeTravelerHohei/pic99.gif" width ="600"/>
-        <slideshow width ="900" :slide="slide"/>
-        </div>
         <div class="skill">
             <skillicon v-for="s in skills" :name="s" />
         </div>
-        <div class="text">
-            縦スクロールのタイムアタックゲームです。<br/><br/>
+        <div class="about">
+            <div class="desc">
+                <div class="to-left">
+                    <h3>DESCRIPTION</h3>
+                </div>
+                <div class="text">
+                    縦スクロールのタイムアタックゲームです。<br /><br />
 
-            スピードアップアイテムで加速したり、タイムダウンアイテムで時間操作することでタイム0の理論値スコアを目指します。<br/>
-            難易度が上がると、ブラックホールに吸い寄せられたり、何が起こるかわからない「時の旅人」と遭遇したりします。<br/>
-            技術的にはセーブデータのread/writeを初めて実装しています。
+                    スピードアップアイテムで加速したり、タイムダウンアイテムで時間操作することでタイム0の理論値スコアを目指します。<br />
+                    難易度が上がると、ブラックホールに吸い寄せられたり、何が起こるかわからない「時の旅人」と遭遇したりします。<br />
+                    技術的にはセーブデータのread/writeを初めて実装しています。
+                </div>
+            </div>
+            <div class="top-media">
+                <img src="@/assets/works/program/game/TimeTravelerHohei/pic99.gif" width="600" />
+            </div>
         </div>
+        <media :slide="slide" />
         <div class="to-left">
             <h3>DEVELOP</h3>
         </div>
@@ -28,7 +35,7 @@
 import workbase from "@/components/works/program/WorkBase.vue"
 import autolink from "@/components/AutoLink.vue"
 import skillicon from "@/components/SkillIconAutoCategory.vue"
-import slideshow from "@/components/Slideshow.vue"
+import media from "@/components/works/program/MediaGallery.vue"
 
 export default {
     name: "TimeTravelerHohei",
@@ -39,7 +46,7 @@ export default {
                 require("@/assets/works/program/game/TimeTravelerHohei/pic02.png"),
                 require("@/assets/works/program/game/TimeTravelerHohei/pic03.png"),
                 require("@/assets/works/program/game/TimeTravelerHohei/pic04.png"),
-            ],            
+            ],
             skills: [
                 "C",
                 "Windows API",
@@ -49,10 +56,10 @@ export default {
     components: {
         autolink,
         skillicon,
-        slideshow,
+        media,
         workbase
     },
-    
+
 };
 </script>
     
