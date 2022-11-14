0b7aca249e531976c5031ffd1e0f381befbe6396
tyanmahou <tyanmahou@gmail.com>
2022-11-15 00:13:12 +0900

    update plane

--- web/src/components/works/program/game/plane.vue
+++ web/src/components/works/program/game/plane.vue
@@ -1,23 +1,30 @@
 <template>
     <workbase>
         <h2>くうこう</h2>
-        <div>
-            <img src="@/assets/works/program/game/plane/pic99.gif" width ="600"/>
-            <slideshow width ="900" :slide="slide"/>
-        </div>
         <div class="skill">
             <skillicon v-for="s in skills" :name="s" />
         </div>
-        <div class="text">
-        8時間ハッカソンで作成したものです。<br/><br/>
+        <div class="about">
+            <div class="desc">
+                <div class="to-left">
+                    <h3>DESCRIPTION</h3>
+                </div>
+                <div class="text">
+                    8時間ハッカソンで作成したものです。<br /><br />
 
-        燃料と残機を維持しながら時間内にゴールを目指します<br/>
+                    燃料と残機を維持しながら時間内にゴールを目指します<br />
+                </div>
+            </div>
+            <div class="top-media">
+                <img src="@/assets/works/program/game/plane/pic99.gif" width="600" />
+            </div>
         </div>
+        <media :slide="slide" />
         <div class="to-left">
             <h3>DEVELOP</h3>
         </div>
         <div class="text">
-            2015 (8時間)<br/><br/>
+            2015 (8時間)<br /><br />
 
             担当：プログラマ
         </div>
@@ -28,7 +35,7 @@
 import workbase from "@/components/works/program/WorkBase.vue"
 import autolink from "@/components/AutoLink.vue"
 import skillicon from "@/components/SkillIconAutoCategory.vue"
-import slideshow from "@/components/Slideshow.vue"
+import media from "@/components/works/program/MediaGallery.vue"
 
 export default {
     name: "plane",
@@ -37,8 +44,8 @@ export default {
             slide: [
                 require("@/assets/works/program/game/plane/pic01.png"),
                 require("@/assets/works/program/game/plane/pic02.png"),
-                require("@/assets/works/program/game/plane/pic03.png"),                
-            ],            
+                require("@/assets/works/program/game/plane/pic03.png"),
+            ],
             skills: [
                 "C++",
                 "Windows API",
@@ -48,10 +55,10 @@ export default {
     components: {
         autolink,
         skillicon,
-        slideshow,
+        media,
         workbase
     },
-    
+
 };
 </script>
     
