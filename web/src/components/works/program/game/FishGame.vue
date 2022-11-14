f2b5788126e7f19623023c8728f61fff347eba9b
tyanmahou <tyanmahou@gmail.com>
2022-11-14 23:22:08 +0900

    update FishGame

--- web/src/components/works/program/game/FishGame.vue
+++ web/src/components/works/program/game/FishGame.vue
@@ -1,24 +1,32 @@
 <template>
     <workbase>
         <h2>FishGame</h2>
-        <div>
-            <img src="@/assets/works/program/game/FishGame/pic99.gif" width ="600"/>
-        </div>
         <div class="skill">
             <skillicon v-for="s in skills" :name="s" />
         </div>
-        <div class="text">
-        8時間ハッカソンで作成したものです。<br/><br/>
+        <div class="about">
+            <div class="desc">
+                <div class="to-left">
+                    <h3>DESCRIPTION</h3>
+                </div>
+                <div class="text">
+                    8時間ハッカソンで作成したものです。<br /><br />
 
-        障害物にあたると減速するので避けていきます。<br/>
-        制限時間内にどれだけ進めるかスコアを競います。<br/>
-        プログラマ3人のチームで初めてのハッカソンでした。
+                    障害物にあたると減速するので避けていきます。<br />
+                    制限時間内にどれだけ進めるかスコアを競います。<br />
+                    プログラマ3人のチームで初めてのハッカソンでした。
+                </div>
+            </div>
+            <div class="top-media">
+                <img src="@/assets/works/program/game/FishGame/pic99.gif" width="600" />
+            </div>
         </div>
+        <media :slide="slide" />
         <div class="to-left">
             <h3>DEVELOP</h3>
         </div>
         <div class="text">
-            2014 (8時間)<br/><br/>
+            2014 (8時間)<br /><br />
             担当：プログラマ
         </div>
     </workbase>
@@ -28,14 +36,14 @@
 import workbase from "@/components/works/program/WorkBase.vue"
 import autolink from "@/components/AutoLink.vue"
 import skillicon from "@/components/SkillIconAutoCategory.vue"
-import slideshow from "@/components/Slideshow.vue"
+import media from "@/components/works/program/MediaGallery.vue"
 
 export default {
     name: "FishGame",
     data() {
         return {
             slide: [
-            ],            
+            ],
             skills: [
                 "C++",
                 "Windows API",
@@ -46,10 +54,10 @@ export default {
     components: {
         autolink,
         skillicon,
-        slideshow,
+        media,
         workbase
     },
-    
+
 };
 </script>
     
