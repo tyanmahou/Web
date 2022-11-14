924a7e87aa9b85d920cfeae24df8ca3e3d93b2ea
tyanmahou <tyanmahou@gmail.com>
2022-11-14 23:54:00 +0900

    update MathP

--- web/src/components/works/program/game/MathP.vue
+++ web/src/components/works/program/game/MathP.vue
@@ -1,25 +1,32 @@
 <template>
     <workbase>
         <h2>数学P-MathP-</h2>
-        <div>
-            <img src="@/assets/works/program/game/MathP/pic99.gif" width="600" />
-            <slideshow width="900" :slide="slide" />
-        </div>
         <div class="skill">
             <skillicon v-for="s in skills" :name="s" />
         </div>
-        <div class="text">
-            マウス操作で点Pをゴールに導くゲームです。<br />
+        <div class="about">
+            <div class="desc">
+                <div class="to-left">
+                    <h3>DESCRIPTION</h3>
+                </div>
+                <div class="text">
+                    マウス操作で点Pをゴールに導くゲームです。<br />
 
-            <br />
-            「GGJ 2019」で3人チームで制作しました。<br/>
+                    <br />
+                    「GGJ 2019」で3人チームで制作しました。<br />
+                </div>
+            </div>
+            <div class="top-media">
+                <img src="@/assets/works/program/game/MathP/pic99.gif" width="600" />
+            </div>
         </div>
+        <media :slide="slide" />
         <div class="to-left">
             <h3>PLAY</h3>
         </div>
         <div class="text">
-            <autolink url="https://globalgamejam.org/2019/games/%E6%95%B0%E5%AD%A6p-mathp"/>
-        </div>        
+            <autolink url="https://globalgamejam.org/2019/games/%E6%95%B0%E5%AD%A6p-mathp" />
+        </div>
         <div class="to-left">
             <h3>DEVELOP</h3>
         </div>
@@ -35,7 +42,7 @@ import workbase from "@/components/works/program/WorkBase.vue"
 import autolink from "@/components/AutoLink.vue"
 import skillicon from "@/components/SkillIconAutoCategory.vue"
 import youtube from "@/components/widgets/Youtube/Embed.vue"
-import slideshow from "@/components/Slideshow.vue"
+import media from "@/components/works/program/MediaGallery.vue"
 
 export default {
     name: "MathP",
@@ -56,7 +63,7 @@ export default {
     components: {
         autolink,
         skillicon,
-        slideshow,
+        media,
         youtube,
         workbase
     },
