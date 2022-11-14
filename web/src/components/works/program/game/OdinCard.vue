3996a7a1e15e0a3a376c3a40035551e16555a7ec
tyanmahou <tyanmahou@gmail.com>
2022-11-15 00:21:54 +0900

    update OdinCard

--- web/src/components/works/program/game/OdinCard.vue
+++ web/src/components/works/program/game/OdinCard.vue
@@ -1,29 +1,36 @@
 <template>
     <workbase>
         <h2>OdinCard</h2>
-        <div>
-            <img src="@/assets/works/program/game/OdinCard/pic99.gif" width ="600"/>
-            <slideshow width="900" :slide="slide" />
-        </div>
         <div class="skill">
             <skillicon v-for="s in skills" :name="s" />
         </div>
-        <div class="text">
-            1つのPCで2人対戦するじゃんけん系の心理戦テーブルゲームです。<br /><br />
+        <div class="about">
+            <div class="desc">
+                <div class="to-left">
+                    <h3>DESCRIPTION</h3>
+                </div>
+                <div class="text">
+                    1つのPCで2人対戦するじゃんけん系の心理戦テーブルゲームです。<br /><br />
 
-            毎ターン互いに手持ちのカードから1枚選んで勝負します。<br />
-            各カードに強さがあり、相手の王のカードを倒すと勝利になります。<br />
-            また、プレイヤーが任意で特殊能力を持ったカードを1枚選ぶことができます。<br />
-            こちらの作品は、部活動として「U-22 プログラミング・コンテスト2014」に提出しています。
+                    毎ターン互いに手持ちのカードから1枚選んで勝負します。<br />
+                    各カードに強さがあり、相手の王のカードを倒すと勝利になります。<br />
+                    また、プレイヤーが任意で特殊能力を持ったカードを1枚選ぶことができます。<br />
+                    こちらの作品は、部活動として「U-22 プログラミング・コンテスト2014」に提出しています。
+                </div>
+            </div>
+            <div class="top-media">
+                <img src="@/assets/works/program/game/OdinCard/pic99.gif" width="600" />
+            </div>
         </div>
+        <media :slide="slide" />
         <div class="to-left">
             <h3>DEVELOP</h3>
         </div>
         <div class="text">
             2014 (数ヵ月)<br /><br />
             プログラマー：mahou<br />
-            デザイン：nobu<br />
-            カードデザイン：weed, arisan<br />
+            デザイン：weed<br />
+            カードデザイン：weed, nobu, arisan<br />
             音楽：rekku<br />
         </div>
     </workbase>
@@ -33,7 +40,7 @@
 import workbase from "@/components/works/program/WorkBase.vue"
 import autolink from "@/components/AutoLink.vue"
 import skillicon from "@/components/SkillIconAutoCategory.vue"
-import slideshow from "@/components/Slideshow.vue"
+import media from "@/components/works/program/MediaGallery.vue"
 import imageModal from "@/components/ImageModal.vue"
 
 export default {
@@ -59,13 +66,9 @@ export default {
     components: {
         autolink,
         skillicon,
-        slideshow,
+        media,
         workbase,
         imageModal
-    },
-    mounted() {
-        console.log(this.$el);
-        this.self = this.$el;
     }
 };
 </script>
