54695a26ba127f99ca1b6871c54ce715534a2f09
tyanmahou <tyanmahou@gmail.com>
2022-11-15 00:28:21 +0900

    update MoguMoguSTG

--- web/src/components/works/program/game/MoguMoguSTG.vue
+++ web/src/components/works/program/game/MoguMoguSTG.vue
@@ -1,18 +1,25 @@
 <template>
     <workbase>
         <h2>もぐもぐSTG</h2>
-        <div>
-        <youtube url="https://www.youtube.com/embed/A94zoT6p80k" />
-        <slideshow width ="900" :slide="slide"/>
-        </div>
         <div class="skill">
             <skillicon v-for="s in skills" :name="s" />
         </div>
-        <div class="text">
-        未完成作品です。<br/><br/>
+        <div class="about">
+            <div class="desc">
+                <div class="to-left">
+                    <h3>DESCRIPTION</h3>
+                </div>
+                <div class="text">
+                    未完成作品です。<br /><br />
 
-        STGゲームの動きなどを実装してみてました。
+                    STGゲームの動きなどを実装してみてました。
+                </div>
+            </div>
+            <div class="top-media">
+                <youtube url="https://www.youtube.com/embed/A94zoT6p80k" />
+            </div>
         </div>
+        <media :slide="slide" />
         <div class="to-left">
             <h3>DEVELOP</h3>
         </div>
@@ -26,7 +33,7 @@
 import workbase from "@/components/works/program/WorkBase.vue"
 import autolink from "@/components/AutoLink.vue"
 import skillicon from "@/components/SkillIconAutoCategory.vue"
-import slideshow from "@/components/Slideshow.vue"
+import media from "@/components/works/program/MediaGallery.vue"
 import youtube from "@/components/widgets/Youtube/Embed.vue"
 
 export default {
@@ -37,7 +44,7 @@ export default {
                 require("@/assets/works/program/game/MoguMoguSTG/pic01.png"),
                 require("@/assets/works/program/game/MoguMoguSTG/pic02.png"),
                 require("@/assets/works/program/game/MoguMoguSTG/pic03.png"),
-            ],            
+            ],
             skills: [
                 "C++",
                 "Windows API",
@@ -47,15 +54,19 @@ export default {
     components: {
         autolink,
         skillicon,
-        slideshow,
+        media,
         youtube,
         workbase
     },
-    
+
 };
 </script>
     
 <style lang="scss" scoped>
-iframe {aspect-ratio:560 / 315;width:100%;height:100%}
+iframe {
+    aspect-ratio: 560 / 315;
+    width: 100%;
+    height: 100%
+}
 </style>
     
