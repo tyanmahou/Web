303f620d531b1d875e580b8bdcae20f65e74e301
tyanmahou <tyanmahou@gmail.com>
2022-11-15 00:18:53 +0900

    update Tome

--- web/src/components/works/program/game/OtomeNoTome.vue
+++ web/src/components/works/program/game/OtomeNoTome.vue
@@ -1,24 +1,27 @@
 <template>
     <workbase>
         <h2>乙女のトメ</h2>
-        <div>
-            <slideshow width="900" :slide="slide" />
-        </div>
         <div class="skill">
             <skillicon v-for="s in skills" :name="s" />
         </div>
-        <div class="text">
-            おばあちゃんの「トメ」をタイムマシンで時空を移動することで若返らせるゲームです。<br/>
-            部活動の48時間ゲームジャムでプログラマ3人、サウンド2人の5人チームで作成しました。<br/><br/>
+        <div class="about">
+            <div class="to-left">
+                <h3>DESCRIPTION</h3>
+            </div>
+            <div class="text">
+                おばあちゃんの「トメ」をタイムマシンで時空を移動することで若返らせるゲームです。<br />
+                部活動の48時間ゲームジャムでプログラマ3人、サウンド2人の5人チームで作成しました。<br /><br />
 
-            最初にどの時期まで若返らせるかステージを選びます。<br/>
-            時空ではタイムマシンがどんどん加速するので、目標の年齢でうまくタイムマシンを止めれるように、<br/>
-            スピード変化アイテムを取得しましょう。<br/>
-            また、爆弾にあたるとダメージを受けるので避けましょう。<br/>
-            上手く目標成功した場合は、到達時間がランキングに登録されます。<br/>
+                最初にどの時期まで若返らせるかステージを選びます。<br />
+                時空ではタイムマシンがどんどん加速するので、目標の年齢でうまくタイムマシンを止めれるように、<br />
+                スピード変化アイテムを取得しましょう。<br />
+                また、爆弾にあたるとダメージを受けるので避けましょう。<br />
+                上手く目標成功した場合は、到達時間がランキングに登録されます。<br />
 
-            サウンドが2人いたのを活かして、各年代ごとにクリアジングルをわけています。
+                サウンドが2人いたのを活かして、各年代ごとにクリアジングルをわけています。
+            </div>
         </div>
+        <media :slide="slide" />
         <div class="to-left">
             <h3>DEVELOP</h3>
         </div>
@@ -37,7 +40,7 @@ import workbase from "@/components/works/program/WorkBase.vue"
 import autolink from "@/components/AutoLink.vue"
 import skillicon from "@/components/SkillIconAutoCategory.vue"
 import youtube from "@/components/widgets/Youtube/Embed.vue"
-import slideshow from "@/components/Slideshow.vue"
+import media from "@/components/works/program/MediaGallery.vue"
 
 export default {
     name: "OtomeNoTome",
@@ -59,7 +62,7 @@ export default {
     components: {
         autolink,
         skillicon,
-        slideshow,
+        media,
         youtube,
         workbase
     },
