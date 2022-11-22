ede9e184049dcf55af18d0e400fbf6152be579d7
tyanmahou <tyanmahou@gmail.com>
2022-11-22 23:39:08 +0900

    update home works

--- web/src/components/home/WorkGame.vue
+++ web/src/components/home/WorkGame.vue
@@ -1,21 +1,29 @@
 <template>
-    <mylink url="https://colorfultone.tyanmahou.com/web/" absolute target="_blank">
-        <mycaption title="ColorfulTone" caption="音楽ゲーム">
-            <img src="@/assets/works/program/game/ColorfulTone/Screenshot1.png" width="400">
-        </mycaption>
-    </mylink>
+    <div class="work-game">
+        <mylink url="https://colorfultone.tyanmahou.com/web/" absolute target="_blank">
+            <div class="work-img">
+                <img src="@/assets/works/program/game/ColorfulTone/Screenshot1.png">
+            </div>
+            <div class="detail">
+                <p class="title">
+                    ColorfulTone
+                </p>
+                <p class="caption">
+                    音楽ゲーム
+                </p>
+            </div>
+        </mylink>
+    </div>
 </template>
   
 <script>
 import mylink from "@/components/Link.vue";
-import mycaption from "@/components/FigCaption.vue";
 
 export default {
     name: "WorkGame",
     props: {},
     components: {
         mylink,
-        mycaption,
     },
     data() {
         return {};
@@ -26,10 +34,60 @@ export default {
 <style lang="scss" scoped>
 @import "@/scss/common";
 
+.work-game {
+margin: 0 5px 0 0;
+display: inline-block;
+width: 350px;
 @media only screen and (max-width: $layout-min-width) {
+    width: 100%;
+}
+vertical-align: top;
+
+.work-img {
+  display: table-cell;
+  vertical-align: middle;
+  overflow: hidden;
+  text-align: center;
+  background-color: #fff;
+
+  img {
+    object-fit: cover;
+    vertical-align: middle;
+    transition: .2s;
+    width: 100%;
+    height: 100%;
+  }
+}
+
+.detail {
+  position: relative;
+  text-align: left;
+
+  .title {
+    font-size: 20px;
+    font-weight: bold;
+    transition: .2s;
+  }
+
+  .caption {
+    font-size: 12px;
+  }
+}
+
+&:hover {
+  .work-img {
     img {
-        width: 100%;
+      opacity: 0.7;
+      scale: 105%;
     }
+  }
+
+  .detail {
+    .title {
+      color: $color-theme;
+    }
+  }
+}
 }
 </style>
   
