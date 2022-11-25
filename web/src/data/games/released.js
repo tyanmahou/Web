b7f71a01ca83b4d70756e178fb82684a541ffb79
tyanmahou <tyanmahou@gmail.com>
2022-11-25 12:17:59 +0900

    update released

--- web/src/data/games/released.js
+++ web/src/data/games/released.js
@@ -1,2 +1,14 @@
+import { shallowRef } from 'vue'
+
+// Game
+import gameColorfulTone from "@/components/works/program/game/ColorfulTone.vue"
+
 export default [
+    {
+        title: "ColorfulTone",
+        caption: "音楽ゲーム",
+        img: require("@/assets/works/program/game/ColorfulTone/Screenshot1.png"),
+        detail: shallowRef(gameColorfulTone),
+        playable: true,
+    },
 ];
--- web/src/views/Games.vue
+++ web/src/views/Games.vue
@@ -5,28 +5,38 @@
       <h1>GAMES</h1>
       <h2>UPCOMING</h2>
       <lsma :element-width="310" :margin-offset="20" class="contents-wrap">
-        Comming Soon      
+        Comming Soon
         <ul>
-          <li v-for="item in upcoming">
-            <mymodel>
-              <template #title>
-                <mycaption :title="item.title" :caption="item.caption">
-                  <div class="work">
-                    <img :src="item.img" width="300">
-                  </div>
-                </mycaption>
-              </template>
-              <template #contents>
-                <component :is="item.detail" />
-              </template>
-            </mymodel>
-          </li>
         </ul>
       </lsma>
       <h2>RELEASED</h2>
       <lsma :element-width="310" :margin-offset="20" class="contents-wrap">
-        Comming Soon
-        <ul>
+        <ul class="game-list">
+          <li v-for="item in released">
+            <mymodal>
+              <template #title>
+                <div class="work-img">
+                  <img :src="item.img" width="300">
+                </div>
+                <div class="detail">
+                  <p class="title">
+                    {{ item.title }}
+                  </p>
+                  <p class="caption">
+                    {{ item.caption }}
+                  </p>
+                  <p v-if="item.event" class="event">
+                    {{ item.event }}
+                  </p>
+                  <playableicon class="playable-icon" v-if="item.playable" />
+                </div>
+                <hr />
+              </template>
+              <template #contents>
+                <component :is="item.detail" />
+              </template>
+            </mymodal>
+          </li>
         </ul>
       </lsma>
       <h2>CASUAL</h2>
@@ -34,7 +44,7 @@
         Comming Soon
         <ul>
         </ul>
-      </lsma>      
+      </lsma>
     </div>
   </div>
   <foot />
@@ -45,6 +55,7 @@ import myheader from "@/components/Header.vue";
 import foot from "@/components/Foot.vue";
 import mylink from "@/components/Link.vue"
 import mymodal from "@/components/Modal.vue"
+import playableicon from "@/components/works/program/PlayableIcon.vue"
 import lsma from "@/components/ListSideMarginAdjuster.vue"
 
 import upcoming from "@/data/games/upcoming";
@@ -57,7 +68,7 @@ export default {
     return {
       upcoming: upcoming,
       released: released,
-      casual : casual ,
+      casual: casual,
     };
   },
   components: {
@@ -65,7 +76,8 @@ export default {
     foot,
     mylink,
     mymodal,
-    lsma,    
+    playableicon,
+    lsma,
   },
   mounted() {
   }
@@ -76,31 +88,98 @@ export default {
 @import "@/scss/common";
 
 ul {
-  text-align: left;
+  hr {
+    border-top: 1px solid $color-theme;
+  }
+}
+
+ul.game-list{
   list-style: none;
-  //transition: all .3s;
+  text-align: left;
 
   @media only screen and (max-width: $layout-mobile) {
     text-align: center;
   }
 
   li {
+
     display: inline-block;
     margin: 5px;
-  }
-}
 
-.work {
-  display: table-cell;
-  vertical-align: middle;
-  width: 300px;
-  height: 225px;
-  overflow: hidden;
-  text-align: center;
-  img {
-    max-width: 100%;
-    max-height: 100%;
-    object-fit: cover;
+    width: 300px;
+    vertical-align: top;
+
+    .work-img {
+      display: table-cell;
+      vertical-align: middle;
+      width: 300px;
+      height: 225px;
+      overflow: hidden;
+      text-align: center;
+      background-color: #fff;
+
+      img {
+        object-fit: cover;
+        vertical-align: middle;
+        transition: .2s;
+      }
+
+      img.game-img {
+        width: 100%;
+        height: 100%;
+      }
+
+      img.web-img {
+        max-width: 100%;
+        max-height: 100%;
+      }
+    }
+
+    .detail {
+      position: relative;
+      height: 70px;
+      text-align: left;
+
+      .title {
+        font-size: 15px;
+        font-weight: bold;
+        transition: .2s;
+      }
+
+      .caption {
+        font-size: 12px;
+      }
+
+      .event {
+        display: inline;
+        color: #fff;
+        background-color: #aaa;
+        font-size: 10px;
+        padding: 2px;
+        border-radius: 7px;
+      }
+
+      .playable-icon {
+        position: absolute;
+        top: 28px;
+        right: 5px;
+      }
+    }
+
+    &:hover {
+      .work-img {
+        img {
+          opacity: 0.7;
+          scale: 105%;
+        }
+      }
+
+      .detail {
+        .title {
+          color: $color-theme;
+        }
+      }
+    }
   }
 }
 </style>
