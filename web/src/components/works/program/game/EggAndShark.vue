7597d06ec00f4531b2dc4a7c470e888d82ac4695
tyanmahou <tyanmahou@gmail.com>
2022-12-06 23:03:34 +0900

    update EggAndShark

--- web/src/components/works/program/game/EggAndShark.vue
+++ web/src/components/works/program/game/EggAndShark.vue
@@ -1,7 +1,7 @@
 <template>
     <workbase>
         <h2>ゆでたまごとさめ</h2>
-        <div class="skill">
+        <div class="skill" v-if="detailed">
             <skillicon v-for="s in skills" :name="s" />
         </div>
         <div class="about">
@@ -9,7 +9,7 @@
                 <div class="to-left">
                     <h3>DESCRIPTION</h3>
                 </div>
-                <div class="text">
+                <div class="text" v-if="detailed">
                     お客さんの要望どおりのゆでぐあいのたまごをクリックするゲームです。<br /><br />
 
                     ゆでたまごはゆですぎると死んでしまうし完熟たまごとはいえゆですぎてもダメ。<br>
@@ -18,26 +18,38 @@
                     <br />
                     「第1回Siv3DGameJam」投稿作品です。<br />
                 </div>
+                <div class="text" v-else>
+                    お客さんの要望どおりのゆでぐあいのたまごをクリックするゲームです。<br /><br />
+
+                    ゆでたまごはゆですぎると死んでしまうし完熟たまごとはいえゆですぎてもダメ。<br>
+                    自分の中でたまごのゆでぐあいを覚えていこう。<br>
+                    サメはゆでたまごを食べてしまうのでクリックで攻撃して倒そう。
+                </div>                
             </div>
             <div class="top-media">
                 <img src="@/assets/works/program/game/EggAndShark/pic99.gif" width="600" />
             </div>
         </div>
         <media :slide="slide" />
-        <div class="to-left">
-            <h3>PLAY</h3>
+        <div>
+            <div class="to-left">
+                <h3>PLAY</h3>
+            </div>
+            <div class="text">
+                <a href="http://ux.getuploader.com/tyanmahou_game/download/3/egg_and_shark.zip"
+                    target="blank">ダウンロード</a>
+            </div>
         </div>
-        <div class="text">
-            <a href="http://ux.getuploader.com/tyanmahou_game/download/3/egg_and_shark.zip" target="blank">ダウンロード</a>
-        </div>
-        <div class="to-left">
-            <h3>DEVELOP</h3>
-        </div>
-        <div class="text">
-            2016 (1日)<br /><br />
-            プログラマー：mahou<br>
-            デザイン：udon<br>
-            サウンド：naotit
+        <div v-if="detailed">
+            <div class="to-left">
+                <h3>DEVELOP</h3>
+            </div>
+            <div class="text">
+                2016 (1日)<br /><br />
+                プログラマー：mahou<br>
+                デザイン：udon<br>
+                サウンド：naotit
+            </div>
         </div>
     </workbase>
 </template>
@@ -51,6 +63,12 @@ import media from "@/components/works/program/MediaGallery.vue"
 
 export default {
     name: "EggAndShark",
+    props: {
+        detailed: {
+            type: Boolean,
+            default: true
+        }
+    },
     data() {
         return {
             slide: [
